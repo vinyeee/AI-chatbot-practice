99b9f011c9540137d98213424959e8afa5ff0e48
vinyeee <loveyebin077@naver.com>
2023-05-02 01:29:28 +0900

    3 weeks chatbot study

---
+++ chatbot/test/chatbot_dict_test.py
@@ -0,0 +1,26 @@
+import sys
+import pickle
+from utils.Preprocess import Preprocess
+
+sys.path.append('c:/Users/YB/Desktop/chatbot') 
+# 단어 사전 불러오기
+f = open("c:/Users/1dlwk/Desktop/GnB/ai_project/chatbot/chatbot_dict.bin", "rb")
+word_index = pickle.load(f)
+f.close()
+
+sent = input()
+
+# 전처리 객체 생성
+p = Preprocess(userdic='c:/Users/1dlwk/Desktop/GnB/ai_project/chatbot/utils/user_dic.tsv')
+
+# 형태소 분석기 실행
+pos = p.pos(sent)
+
+# 품사 태그 없이 키워드 출력
+keywords = p.get_keywords(pos, without_tag=True)
+for word in keywords:
+    try:
+        print(word, word_index[word])
+    except KeyError:
+        # 해당 단어가 사전에 없는 경우 OOV 처리
+        print(word, word_index['OOV'])
